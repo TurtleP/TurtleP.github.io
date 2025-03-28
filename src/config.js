14e739c22bd449982ae441d0e8b6ed10e9508724
TurtleP <jpostelnek@outlook.com>
2025-03-27 22:39:58 -0400

    push changes

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 export const config = {
-    title: "Serena's Blog",
-    favicon: "/avatar.jpg",
-    subtitle: "Thoughts, stories, and ideas.",
-    profileImage: "/avatar.jpg",
-    links: [
-        { name: "Home", href: "", match: "^/$" },
-        { name: "Archive", href: "archive/1",  match: "^/archive|^/posts" },
-        { name: "VRChat Memories", href: "gallery/1", match: "^/gallery" },
-        { name: "About", href: "about", match: "^/about" },
-    ]
+  title: "Serena's Blog",
+  favicon: '/avatar.jpg',
+  subtitle: 'Thoughts, stories, and ideas.',
+  profileImage: '/avatar.jpg',
+  links: [
+    { name: 'Home', href: '/' },
+    { name: 'Archive', href: '/archive/1' },
+    { name: 'VRChat Memories', href: '/gallery/1' },
+    { name: 'About', href: '/about' },
+  ],
 };
